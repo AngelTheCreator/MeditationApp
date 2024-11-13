9e89f55280d3bd2d05bcd87980ffc955333bc521
AngelTheCreator <angel_guevara94@hotmail.com>
2024-11-13 15:30:21 -0800

    native wind set up

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
-export default function Page() {
+export default function HomeScreen() {
   return (
     
       <View>
-        <Text>HomeScreen</Text>
+        <Text className="font-bold text-4xl p-10 bg-red-500 color-blue-500">HomeScreen</Text>
       </View>
   );
 }
