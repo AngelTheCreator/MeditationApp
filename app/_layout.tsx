070f999b195c69f017a05893fa1fb10b041a9a4b
AngelTheCreator <angel_guevara94@hotmail.com>
2024-12-11 18:06:16 -0800

    installed and used prettier on all relevant files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,14 @@
-import {Slot, Stack } from 'expo-router';
-import '../global.css';
+import { Slot, Stack } from "expo-router";
+import "../global.css";
 
 export default function RootLayout() {
-    return(
+  return (
     <Stack>
-        <Stack.Screen name='index' options={{title: 'Meditations'}}/>   
-        <Stack.Screen 
-        name='meditation/[id]' 
-        options={{headerShown: false, animation: 'slide_from_bottom'}}/>   
+      <Stack.Screen name="index" options={{ title: "Meditations" }} />
+      <Stack.Screen
+        name="meditation/[id]"
+        options={{ headerShown: false, animation: "slide_from_bottom" }}
+      />
     </Stack>
-)
+  );
 }
--- app/meditation/[id].tsx
+++ app/meditation/[id].tsx
@@ -1,31 +1,38 @@
 import { useLocalSearchParams, router } from "expo-router";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { meditations } from "@/data";
-import AntDesign from '@expo/vector-icons/AntDesign';
-import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import {
+  SafeAreaView,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
 export default function MeditationDetails() {
-    const { id } = useLocalSearchParams<{ id: string}>();
+  const { id } = useLocalSearchParams<{ id: string }>();
 
-    const {top} = useSafeAreaInsets();
+  const { top } = useSafeAreaInsets();
 
-    const meditation = meditations.find((m) => m.id === Number(id));
+  const meditation = meditations.find((m) => m.id === Number(id));
 
-    if (!meditation) {
-        return <Text>Meditation not found!</Text>
-    }
+  if (!meditation) {
+    return <Text>Meditation not found!</Text>;
+  }
 
-    return (
-    <SafeAreaView>
-        <Text className="text-3xl mt-14">{meditation?.title}: {top}</Text>
-        <AntDesign 
-        onPress={() => router.back()}
-        className="absolute right-4"
-        style={{top: top + 50}}
-        name="close" 
-        size={24} 
-        color="black" />
-        
+  return (
+    <SafeAreaView className="bg-orange-400 flex-1">
+      {/* Header */}
+      <View>
+        <AntDesign name="infocirlceo" size={24} color="black" />
+        <AntDesign
+          onPress={() => router.back()}
+          name="close"
+          size={24}
+          color="black"
+        />
+      </View>
+      <Text className="text-3xl mt-14">
+        {meditation?.title}: {top}
+      </Text>
     </SafeAreaView>
-    )
+  );
 }
