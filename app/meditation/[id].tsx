b602e23b96539fbcab36eb83117a1eba21bb0469
AngelTheCreator <angel_guevara94@hotmail.com>
2024-12-11 17:54:24 -0800

    added close button to mediation details view

--- app/meditation/[id].tsx
+++ app/meditation/[id].tsx
@@ -1,9 +1,13 @@
-import { useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, router } from "expo-router";
 import { Text } from "react-native";
 import { meditations } from "@/data";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function MeditationDetails() {
     const { id } = useLocalSearchParams<{ id: string}>();
+
+    const {top} = useSafeAreaInsets();
 
     const meditation = meditations.find((m) => m.id === Number(id));
 
@@ -11,5 +15,17 @@ export default function MeditationDetails() {
         return <Text>Meditation not found!</Text>
     }
 
-    return <Text className="text-3xl">{meditation?.title}</Text>
+    return (
+    <SafeAreaView>
+        <Text className="text-3xl mt-14">{meditation?.title}: {top}</Text>
+        <AntDesign 
+        onPress={() => router.back()}
+        className="absolute right-4"
+        style={{top: top + 50}}
+        name="close" 
+        size={24} 
+        color="black" />
+        
+    </SafeAreaView>
+    )
 }
